67eb36cce471bcf5d44f2a7f43e28ca35c9e0c38
jiyinyiyong <jiyinyiyong@gmail.com>
2017-05-30 23:59:59 +0800

    fallback require syntax

--- src/client/main.cljs
+++ src/client/main.cljs
@@ -1,8 +1,9 @@
 
 (ns client.main
   (:require [client.lib :as lib]
-            [hsl.core :as hsl-core]
-            ["hsl" :as npm-hsl]))
+            [hsl.core :as hsl-core]))
+
+(def npm-hsl (js/require "hsl"))
 
 ; for requiring npm package
 ; https://github.com/thheller/shadow-cljs/issues/10#issuecomment-302373576
@@ -12,7 +13,7 @@
 (defn render! []
   (println lib/lib-data)
   (println "Trying jar" (hsl-core/hsl 10 20 30 0.3))
-  (println "Trying npm" (npm-hsl.hsl 10 20 30 0.3))
+  (println "Trying npm" (npm-hsl 10 20 30 0.3))
   (set! js/document.body.innerHTML lib/lib-data))
 
 (defn init []
