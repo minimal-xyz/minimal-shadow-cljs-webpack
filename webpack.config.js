b450c04a8ebeb8b16de2324fad2ef8f837c95bba
ChenYong <yong.chen04@ele.me>
2017-06-06 17:23:49 +0800

    add sourcemaps; format webpack config with prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,29 @@
-
-var path = require("path");
-var resolve = require("path").resolve;
-var webpack = require("webpack");
+var path = require('path');
+var resolve = require('path').resolve;
+var webpack = require('webpack');
 
 module.exports = {
   entry: {
-    main: "./lib/page.js"
+    main: './lib/page.js'
   },
   devServer: {
     stats: 'errors-only',
     contentBase: resolve(__dirname, 'dist'),
-    publicPath: "/"
+    publicPath: '/'
   },
+  devtool: 'source-map',
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: "[name].js"
+    filename: '[name].js'
   },
   module: {
     rules: [
-      {test: /\.css$/, loaders: ['style-loader', 'css-loader']}
+      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] },
+      {
+        test: /\.js$/,
+        loader: 'source-map-loader',
+        options: { enforce: 'pre' }
+      }
     ]
   },
   plugins: [
